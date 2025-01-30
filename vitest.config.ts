56c2e0d0951662936ce9ea39a616dc0d3a83f523
Jadi Miller <jadilmiller@gmail.com>
2025-01-30 12:53:46 -0500

    Add coverage reporting to project

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'istanbul'
+        enabled: true,
+        reporter: ['text', 'json', 'html'],
+        reportsDirectory: './tests/coverage'
+      },
     },
-  },
-})
+  })
