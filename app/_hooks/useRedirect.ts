cbba618c3b9b955a06280f8f3fb9aaa800ea2c36
Pierson Silver <silverpierson1@gmail.com>
2025-03-17 15:20:28 -0400

    Fix how links are handled within buttons to ExternalURLs and Page Paths

--- app/_hooks/useRedirect.ts
+++ app/_hooks/useRedirect.ts
@@ -1,8 +1,12 @@
 import { windowOpen } from "@/_utils/helpers";
 
 export default function useRedirect() {
-  const handleRedirect = (url: string): void => {
-    windowOpen(url);
+  const handleRedirect = (url: string, newTab: boolean = false): void => {
+    if (newTab) {
+      windowOpen(url, "_blank");
+    } else {
+      windowOpen(url, "_self");
+    }
   };
 
   return { handleRedirect };
