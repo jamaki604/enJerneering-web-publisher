f89aa6f79a77055fb907ad49b3bc5ec74798e9e3
Jadi <jadilmiller@gmail.com>
2025-04-21 23:16:20 -0400

    Add picture section

--- app/[pageTitle]/page.tsx
+++ app/[pageTitle]/page.tsx
@@ -8,6 +8,7 @@ import ContactType1 from "@components/Contact/_ContactType1";
 import CallToActionType1 from "@components/CallToAction/_CallToActionType1";
 import MainContentType1 from "@components/MainContent/_MainContentType1";
 import TextBoxType from "@components/TextBox/_TextBox";
+import PictureType from "@components/Picture/_Picture";
 import Header from "@components/Header";
 import CallToAction from "@components/CallToAction";
 import Contact from "@components/Contact";
@@ -104,6 +105,8 @@ const PageViewer = () => {
             return <Contact type={layer.styleVariant} data={layerContent} />;
           case "TextBox":
             return <TextBoxType key={layer.componentType} data={layerContent} />;
+          case "Picture":
+            return <PictureType key={layer.componentType} data={layerContent} />;
           default:
             return null;
         }
